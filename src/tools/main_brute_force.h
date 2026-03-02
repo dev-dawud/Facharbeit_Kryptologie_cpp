69435d66805dff6f6678196517582bcabc1c086a
David Wahab <davidwahab2008@gmail.com>
2026-03-02 21:19:34 +0100

    update

---
+++ src/tools/main_brute_force.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "../../include/picosha2.h"
+#include "../../include/md5.h"
+#include "main_hack.h"
+
+class BruteForce {
+public:
+
+    void erkenneHash() {
+
+        mainTool maintool;
+        int typ = 0;
+        std::string hashPassword, passwordVersuch;
+
+
+        std::string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+
+
+        std::cout << "Bitte geben sie den Hash ein der geprüft werden soll: ";
+
+        std::cin >> hashPassword;
+
+        if (hashPassword.length() == 32) {
+
+            typ = 32;
+
+            std::cout << "hash ist MD5\n";
+        }
+        else if (hashPassword.length() == 64) {
+
+            typ = 64;
+
+            std::cout << "hahs ist SHA-256\n";
+        }
+        else {
+
+            std::cout << "Fehler: Ungueltige Hash laenge!\n";
+
+            return;
+        }
+
+		maintool.bruteForce(hashPassword, typ);
+    }
+};
---
+++ src/tools/main_rainbow_table.h
@@ -0,0 +1,28 @@
+#pragma	once
+
+#include <iostream>
+#include <fstream>
+
+class RainbowTable {
+public: 
+
+	void rainbowTable() {
+
+		// Für die API muss man die dazugehörige E-Mail und den personalisieren Code eingeben in der URL
+		std::string email = "davidwahab869@gmail.com";
+		std::string code = "61f152f62c4a29f6";
+
+		std::string hash;
+
+		std::cout << "Bitte gib denn Hash ein: ";
+		std::cin >> hash;
+
+		// Hier wird die API genutzt um den Hash zu entschlüsseln
+		std::string url = "https://md5decrypt.net/en/Api/api.php?hash=" + hash + "&hash_type=md5&email=" + email + "&code=" + code;
+
+		// da die URL sonderzeichen hat muss man diese in " setzen damit der Link klappt
+		std::system(("start \"\" \"" + url + "\"").c_str());
+
+	}
+};
+
